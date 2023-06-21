3cc2a0d26d3a06418bb5c7cf52f1ce1a8c752732
Bharadwaj Nallavelli <nallavellibharadwaj@outlook.com>
2023-06-20 20:54:00 -0400

    Create add_two_numbers.py

---
+++ add_two_numbers.py
@@ -0,0 +1,7 @@
+# Python program to add two numbers
+def add_two(num1, num2):
+  print(f"The sum of two numbers are: {num1 + num2}")
+
+number1 = eval(input("Enter your number1: "))
+number2 = eval(input("Enter your number2: "))
+add_two(number1, number2)
